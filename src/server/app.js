1ec8f7a0e2ce791f36a603be5f543ba94c934bb6
04Samir <me@samir.cx>
2024-02-21 22:17:00 +0000

    Add BasePath to Static Files

--- src/server/app.js
+++ src/server/app.js
@@ -17,12 +17,12 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.set('views', 'src/assets/views');
 app.engine('html', ejs.renderFile);
-app.use(express.static('src/assets/public'));
+app.use(baseURl, express.static('src/assets/public'));
 
-app.use('/css', express.static('node_modules/bootstrap/dist/css'));
-app.use('/js', express.static('node_modules/bootstrap/dist/js'));
-app.use('/js', express.static('node_modules/jquery/dist'));
-app.use('/js', express.static('node_modules/jquery-ui/dist'));
+app.use(`${baseURl}/css`, express.static('node_modules/bootstrap/dist/css'));
+app.use(`${baseURl}/js`, express.static('node_modules/bootstrap/dist/js'));
+app.use(`${baseURl}/js`, express.static('node_modules/jquery/dist'));
+app.use(`${baseURl}/js`, express.static('node_modules/jquery-ui/dist'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
